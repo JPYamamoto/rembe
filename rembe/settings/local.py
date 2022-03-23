2aa6d422318a75d01d03197a24e67d42837923e1
Juan Pablo Yamamoto <jpyamamoto@ciencias.unam.mx>
2022-03-22 23:54:19 -0600

    Error en la configuración para GH actions

--- rembe/settings/local.py
+++ rembe/settings/local.py
@@ -9,17 +9,6 @@ SECRET_KEY = 'django-insecure-i-oass(559zhi#-3g(p$ot5x=kf!w-qj(2_a-*g__l%7)21aqd
 
 ENV_ALLOWED_HOSTS = ['localhost',]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASS'),
-        'HOST': env('DATABASE_HOST'),
-        'PORT': env('DATABASE_PORT'),
-    }
-}
-
 if env('GH_ACTIONS'):
     from pathlib import Path
 
@@ -29,5 +18,16 @@ if env('GH_ACTIONS'):
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': env('DATABASE_NAME'),
+            'USER': env('DATABASE_USER'),
+            'PASSWORD': env('DATABASE_PASS'),
+            'HOST': env('DATABASE_HOST'),
+            'PORT': env('DATABASE_PORT'),
         }
     }
