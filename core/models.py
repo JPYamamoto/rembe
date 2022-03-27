5c95ba4d00ab2e500217e6db3325b48b6f8b40c1
cocisran <fcoemmanueldmm@gmail.com>
2022-03-26 22:32:18 -0600

    add: Modelo de tarjeta

--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tarjeta(models.Model):
+    NombreTarjeta = models.CharField(max_length=50)
+    FechaVencimiento = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.NombreTarjeta
+
+
+class cuerpoTarjeta(models.Model):
+    Tarjeta = models.ForeignKey(Tarjeta, on_delete=models.CASCADE)
+    FechaModificacion = models.DateTimeField(auto_now=True)
+    Contenido = models.TextField(blank=True)
+    ContenidoReverso = models.TextField(blank=True)
+
+    def __str__(self):
+        return "Ultima modificacion: \n" + str(self.FechaModificacion) + "\n***Frente\n" + self.Contenido + "\n***ParteTrasera\n" + self.ContenidoReverso
