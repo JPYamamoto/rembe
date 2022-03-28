72344f266cc09b5e2bfa29a4e4fbb7877f9f6144
DavidHdezU <Dhdezu@ciencias.unam.mx>
2022-03-27 19:09:09 -0600

    Añadiendo views para los modelos...

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView
+from core.models import Tarjeta, CuerpoTarjeta
 
 # Create your views here.
+
+
+class TarjetaView(ListView):
+    model = Tarjeta
+    
+class CuerpoTarjetaView(ListView):
+    model = CuerpoTarjeta
