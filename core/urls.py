c7e3a7372ccb551eaeb40fb4ad41e954ef2b748e
Juan Pablo Yamamoto <jpyamamoto@ciencias.unam.mx>
2022-04-14 14:05:27 -0500

    Agregué más vistas para tarjetas.

--- core/urls.py
+++ core/urls.py
@@ -4,9 +4,9 @@ from core import views
 
 app_name = 'core'
 urlpatterns = [
-    # url(r'^$', views.ListarTarjeta.as_view(), name='index'),
+    path('', views.ListarTarjeta.as_view(), name='index'),
     path('create/', views.CrearTarjeta.as_view(), name='create'),
     # url(r'^edit/(?P<pk>\d+)/$', views.EditarTarjeta.as_view(), name='edit'),
-    # url(r'^delete/(?P<pk>\d+)/$', views.EliminarTarjeta.as_view(), name='delete'),
-    # url(r'^view/(?P<pk>\d+)/$', views.show_post, name='view'),
+    path('delete/<pk>/', views.EliminarTarjeta.as_view(), name='delete'),
+    path('view/<pk>/', views.VerTarjeta.as_view(), name='view'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
 from django.urls import reverse_lazy, resolve
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from django.views.generic import CreateView, UpdateView, DeleteView, ListView
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
 
 from core.models import Tarjeta
 from core.forms import TarjetaForm
 
 # Create your views here.
+
+
+class ListarTarjeta(LoginRequiredMixin, ListView):
+    model = Tarjeta
+    template_name = 'tarjeta/list.html'
+
+    def get_queryset(self):
+        return self.model.objects.filter(autor=self.request.user)
+
+
+class VerTarjeta(LoginRequiredMixin, DetailView):
+    model = Tarjeta
+    template_name = 'tarjeta/view.html'
+
+    def get_queryset(self):
+        return self.model.objects.filter(autor=self.request.user)
 
 
 class CrearTarjeta(LoginRequiredMixin, CreateView):
@@ -19,10 +36,13 @@ class CrearTarjeta(LoginRequiredMixin, CreateView):
     template_name = 'tarjeta/form.html'
     success_url = reverse_lazy('tarjetas:index')
 
-class CrearTarjeta(LoginRequiredMixin, CreateView):
-    queryset = Tarjeta.objects.all()
-    login_url = reverse_lazy('auth:login')
+    def form_valid(self, form):
+        form.instance.autor = self.request.user
+        form.instance.fecha_modificacion = timezone.now()
+        return super(CrearTarjeta, self).form_valid(form)
+
+class EliminarTarjeta(LoginRequiredMixin, DeleteView):
+    login_url = '/accounts/login'
     model = Tarjeta
-    form_class = TarjetaForm
-    template_name = 'tarjeta/form.html'
+    template_name = 'tarjeta/delete_form.html'
     success_url = reverse_lazy('tarjetas:index')
