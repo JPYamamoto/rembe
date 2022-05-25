86f083a9df04c1ac572f2dc123361e609b70c082
Juan Pablo Yamamoto <jpyamamoto@ciencias.unam.mx>
2022-05-24 23:22:11 -0500

    Agregamos 4 temas y los integramos en el formulario y en las plantillas.
    Co-authored-by: cocisran <52835031+cocisran@users.noreply.github.com>

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 from markdownx.models import MarkdownxField
 import reversion
 
@@ -16,6 +17,15 @@ class Tarjeta(models.Model):
     fecha_modificacion = models.DateTimeField(auto_now=True)
     contenido = MarkdownxField(blank=False)
     contenido_reverso = MarkdownxField(blank=True)
+
+    class Tema(models.IntegerChoices):
+        DEFAULT  = 0, _('Default')
+        SPLENDOR = 1, _('Splendor')
+        RETRO    = 2, _('Retro')
+        AIR      = 3, _('Air')
+        MODEST   = 4, _('Modest')
+
+    tema = models.IntegerField(choices=Tema.choices, blank=False, null=False, default=Tema.DEFAULT)
 
     def __str__(self):
         return "{} ({})".format(self.nombre, self.fecha_modificacion)
